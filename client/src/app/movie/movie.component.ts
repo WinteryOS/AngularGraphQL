2c38a58afef723ae0014ca0c6144754da258ed3e
John <jashby@student.neumont.edu>
2022-02-18 08:34:45 -0700

    param data on individual movie

--- client/src/app/movie/movie.component.ts
+++ client/src/app/movie/movie.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
-  styleUrls: ['./movie.component.css']
+  styleUrls: ['./movie.component.css'],
 })
 export class MovieComponent implements OnInit {
-
-  constructor() { }
+  id: String | null = '';
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    console.log(this.id);
   }
-
 }
