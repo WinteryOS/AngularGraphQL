59432ff7eae0178f25134dc43fcdcbc1308de323
John <jashby@student.neumont.edu>
2022-03-09 09:03:19 -0700

    regex for register started

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -12,8 +12,20 @@ import { CREATE_USER } from 'src/graphql';
 export class RegisterComponent implements OnInit {
   constructor(private apollo: Apollo, private router: Router) {}
 
+  errors: any = {
+    username: null,
+    password: null,
+    fname: null,
+    lname: null,
+    street: null,
+    city: null,
+    state: null,
+    zip_code: null,
+    email: null,
+    phone: null,
+  };
+
   contactForm = new FormGroup({
-    //VALIDATION EXAMPLE
     username: new FormControl('', [
       Validators.required,
       Validators.pattern(/[a-zA-Z ]*/),
@@ -36,7 +48,9 @@ export class RegisterComponent implements OnInit {
     ]),
     city: new FormControl('', [
       Validators.required,
-      Validators.pattern(/^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$/),
+      Validators.pattern(
+        /^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$/
+      ),
     ]),
     state: new FormControl('', [
       Validators.required,
@@ -48,7 +62,9 @@ export class RegisterComponent implements OnInit {
     ]),
     email: new FormControl('', [
       Validators.required,
-      Validators.pattern(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/),
+      Validators.pattern(
+        /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+      ),
     ]),
     phone: new FormControl('', [
       Validators.required,
@@ -59,29 +75,58 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {}
 
   onSubmit() {
-    console.log(this.contactForm);
-  //   this.apollo
-  //     .mutate({
-  //       mutation: CREATE_USER,
-  //       variables: {
-  //         input: {
-  //           username: this.contactForm.value.username,
-  //           password: this.contactForm.value.password,
-  //           fname: this.contactForm.value.fname,
-  //           lname: this.contactForm.value.lname,
-  //           street: this.contactForm.value.street,
-  //           city: this.contactForm.value.city,
-  //           state: this.contactForm.value.state,
-  //           zip_code: this.contactForm.value.zip_code,
-  //           email: this.contactForm.value.email,
-  //           phone: this.contactForm.value.phone,
-  //           admin: false,
-  //         },
-  //       },
-  //     })
-  //     .subscribe((res: any) => {
-  //       console.log(res.data.createUser);
-  //       this.router.navigate(['/']);
-  //     });
+    if (this.errorCheck()) {
+      console.log('THERE IS AN ERROR');
+    } else {
+      this.apollo
+        .mutate({
+          mutation: CREATE_USER,
+          variables: {
+            input: {
+              username: this.contactForm.value.username,
+              password: this.contactForm.value.password,
+              fname: this.contactForm.value.fname,
+              lname: this.contactForm.value.lname,
+              street: this.contactForm.value.street,
+              city: this.contactForm.value.city,
+              state: this.contactForm.value.state,
+              zip_code: this.contactForm.value.zip_code,
+              email: this.contactForm.value.email,
+              phone: this.contactForm.value.phone,
+              admin: false,
+            },
+          },
+        })
+        .subscribe((res: any) => {
+          console.log(res.data.createUser);
+          this.router.navigate(['/']);
+        });
+    }
+  }
+
+  errorCheck() {
+    this.errors = {
+      username: this.contactForm.controls.username.errors?.required,
+      password: this.contactForm.controls.password.errors?.required,
+      fname: this.contactForm.controls.fname.errors?.required,
+      lname: this.contactForm.controls.lname.errors?.required,
+      street: this.contactForm.controls.street.errors?.required,
+      city: this.contactForm.controls.city.errors?.required,
+      state: this.contactForm.controls.state.errors?.required,
+      zip_code: this.contactForm.controls.zip_code.errors?.required,
+      email: this.contactForm.controls.email.errors?.required,
+      phone: this.contactForm.controls.phone.errors?.required,
+    };
+
+    let tempArr = [];
+    for (var key in this.errors) {
+      if (!this.errors.hasOwnProperty(key)) continue;
+      var obj = this.errors[key];
+      tempArr.push(obj);
+    }
+    if (tempArr.includes(true)) {
+      return true;
+    }
+    return false;
   }
 }
